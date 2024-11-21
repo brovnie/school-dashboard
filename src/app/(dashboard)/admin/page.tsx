89eb8072614db157ca0e3cb913dedaf4a1b8eb52
Marlena Broniewicz <marlena@Marlenas-MacBook-Air.local>
2024-11-21 16:25:45 +0100

    Added Finance Chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart'
 import CountChart from '@/components/CountChart'
+import FinanceChart from '@/components/FinanceChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
 
@@ -24,6 +25,9 @@ function AdminPage() {
            <div className="w-full lg:w-2/3 h-[450px]">
             <AttendanceChart/>
            </div>
+        </div>
+        <div className="w-full h-[500px]">
+          <FinanceChart/>
         </div>
       </div>
       <div className="w-full lg:w-1/3">
