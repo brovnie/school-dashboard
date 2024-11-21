2c631796795d95af2f65c4bc90a28885465b95cb
Marlena Broniewicz <marlena@Marlenas-MacBook-Air.local>
2024-11-21 14:04:04 +0100

    Added User Cards for admin

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,22 @@
+import UserCard from '@/components/UserCard'
 import React from 'react'
 
 function AdminPage() {
   return (
-    <div>AdminPage</div>
+    <div className="flex p4 flex-col md:flex-row">
+      <div className="w-full lg:w-2/3">
+      {/*User Cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student"/>
+          <UserCard type="teacher"/>
+          <UserCard type="parent"/>
+          <UserCard type="stuff"/>
+        </div>
+
+      </div>
+      <div className="w-full lg:w-1/3">
+      </div>
+    </div>
   )
 }
 
