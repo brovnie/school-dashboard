9546c55bdbfa2d3b296322a89f5e9ab2393eab25
Marlena Broniewicz <marlena@Marlenas-MacBook-Air.local>
2024-11-21 16:38:06 +0100

    Added calendar

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart'
 import CountChart from '@/components/CountChart'
+import EventCalendar from '@/components/EventCalendar'
 import FinanceChart from '@/components/FinanceChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
@@ -30,7 +31,8 @@ function AdminPage() {
           <FinanceChart/>
         </div>
       </div>
-      <div className="w-full lg:w-1/3">
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+      <EventCalendar/>
       </div>
     </div>
   )
