01ce767f7f22fb8e0a9d7c169937ef42c9583ee6
Marlena Broniewicz <bronka1.dev@gmail.com>
2024-11-22 22:41:22 +0100

    Added schedule

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,11 +1,18 @@
 import Announcements from '@/components/Announcements'
+import BigCalendar from '@/components/BigCalendar'
 import EventCalendar from '@/components/EventCalendar'
 import React from 'react'
 
 const StudentPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
-      <div className='w-full xl:w-2/3'></div>
+      <div className='w-full xl:w-2/3'>
+        <div className='h-full bg-white p-4 rounded-md'>
+          <h2 className='text-xl font-semibold'>Schedule (4A)</h2>
+          <BigCalendar/>
+        </div>
+      
+      </div>
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
       <EventCalendar/>
       <Announcements/>
