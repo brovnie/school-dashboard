dba1892d221a31da67335defd45558bcc65cf40d
Marlena Broniewicz <bronka1.dev@gmail.com>
2024-11-25 14:22:58 +0100

    Added Parents page

--- src/app/list/parents/page.tsx
+++ src/app/list/parents/page.tsx
@@ -1,20 +1,17 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, studentsData } from "@/lib/data";
+import { parentsData, role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-type Student = {
+type Parent = {
   id: number;
-  studentId: string;
   name: string;
   email?: string;
-  photo: string;
-  phone?: string;
-  grade: number;
-  class: string;
+  students: string[];
+  phone: string;
   address: string;
 };
 
@@ -24,13 +21,8 @@ const columns = [
     accessor: "info",
   },
   {
-    header: "Student ID",
-    accessor: "studentId",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Grade",
-    accessor: "grade",
+    header: "Student Name",
+    accessor: "students",
     className: "hidden md:table-cell",
   },
   {
@@ -49,36 +41,28 @@ const columns = [
   },
 ];
 
-function StudentListPage() {
-  const renderRow = (item: Student) => (
+function ParentListPage() {
+  const renderRow = (item: Parent) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
     >
       <td className="flex items-center gap-4 p-4">
-        <Image
-          src={item.photo}
-          alt=""
-          width={40}
-          height={40}
-          className="md-hidden xl:block w-10 h-10 rounded-full object-cover"
-        />
         <div className="flex flex-col">
           <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-sm text-gray-500">{item.class}</p>
+          <p className="text-sm text-gray-500">{item?.email}</p>
         </div>
       </td>
-      <td className="hidden md:table-cell">{item.studentId}</td>
-      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.students.join(",")}</td>
       <td className="hidden md:table-cell">{item.phone}</td>
       <td className="hidden md:table-cell">{item.address}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/students/${item.id}`}>
+          <Link href={`/list/parents/${item.id}`}>
             <button className="w-7 h-7 rounded-full flex items-center justify-center bg-sky">
               <Image
                 src="/view.png"
-                alt={`View student ${item.name} `}
+                alt={`View parent ${item.name} `}
                 width={16}
                 height={16}
               />
@@ -88,7 +72,7 @@ function StudentListPage() {
             <button className="w-7 h-7 rounded-full flex items-center justify-center bg-customPurple">
               <Image
                 src="/delete.png"
-                alt={`Delete student ${item.name}`}
+                alt={`Delete parent ${item.name}`}
                 width={16}
                 height={16}
               />
@@ -102,7 +86,7 @@ function StudentListPage() {
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All students</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All parents</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -120,10 +104,10 @@ function StudentListPage() {
           </div>
         </div>
       </div>
-      <Table columns={columns} renderRow={renderRow} data={studentsData} />
+      <Table columns={columns} renderRow={renderRow} data={parentsData} />
       <Pagination />
     </div>
   );
 }
 
-export default StudentListPage;
+export default ParentListPage;
