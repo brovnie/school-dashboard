a280cf569a50dee0a09ec8679f7d3386d0139e52
Marlena Broniewicz <bronka1.dev@gmail.com>
2024-11-28 13:22:58 +0100

    fixed visible scroll

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -117,33 +117,32 @@ const menuItems = [
   },
 ];
 
-
 function Menu() {
   return (
-    <div className="mt-4 text-sm overflow-scroll">
-      {menuItems.map(i => (
+    <div className="mt-4 text-sm overflow-view sticky">
+      {menuItems.map((i) => (
         <div className="flex flex-col gap-2" key={i.title}>
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {i.title}
           </span>
-          {i.items.map(item => {
-            if(item.visible.includes(role)) {
-                return (
-                  <Link 
-                  href={item.href} 
-                  key={item.label} 
-                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md-px-2 rounded-md hover:bg-sky">
-                    <Image src={item.icon} alt="" width={20} height={20} />
-                    <span className="hidden lg:block">{item.label}</span>
-                  </Link>
-                )
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md-px-2 rounded-md hover:bg-sky"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
             }
           })}
         </div>
-      )
-      )}
+      ))}
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
