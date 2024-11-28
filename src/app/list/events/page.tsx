fc5578e922476d5e431220fc12e0f2cfb90c6c82
Marlena Broniewicz <bronka1.dev@gmail.com>
2024-11-28 10:02:02 +0100

    removed unused imports

--- src/app/list/events/page.tsx
+++ src/app/list/events/page.tsx
@@ -1,49 +1,42 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { resultsData, role } from "@/lib/data";
+import { eventsData, role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-type Result = {
+type Event = {
   id: number;
-  subject: string;
+  title: string;
   class: string;
-  teacher: string;
-  student: string;
-  type: "exam" | "assignment";
   date: string;
-  score: number;
+  startTime: string;
+  endTime: string;
 };
 
 const columns = [
   {
-    header: "Subject Name",
-    accessor: "name",
-  },
-  {
-    header: "Student",
-    accessor: "student",
-  },
-  {
-    header: "Score",
-    accessor: "score",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Teacher",
-    accessor: "teacher",
-    className: "hidden md:table-cell",
+    header: "Title",
+    accessor: "title",
   },
   {
     header: "Class",
     accessor: "class",
-    className: "hidden md:table-cell",
   },
   {
     header: "Date",
     accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Start Time",
+    accessor: "startTime",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "End Time",
+    accessor: "endTime",
     className: "hidden md:table-cell",
   },
   {
@@ -52,25 +45,24 @@ const columns = [
   },
 ];
 
-function ResultListPage() {
-  const renderRow = (item: Result) => (
+function EventListPage() {
+  const renderRow = (item: Event) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
     >
-      <td className="flex items-center gap-4 p-4">{item.subject}</td>
-      <td>{item.student}</td>
-      <td className="hidden md:table-cell">{item.score}</td>
-      <td className="hidden md:table-cell">{item.teacher}</td>
-      <td className="hidden md:table-cell">{item.class}</td>
+      <td className="flex items-center gap-4 p-4">{item.title}</td>
+      <td>{item.class}</td>
       <td className="hidden md:table-cell">{item.date}</td>
+      <td className="hidden md:table-cell">{item.startTime}</td>
+      <td className="hidden md:table-cell">{item.endTime}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/results/${item.id}`}>
+          <Link href={`/list/events/${item.id}`}>
             <button className="w-7 h-7 rounded-full flex items-center justify-center bg-sky">
               <Image
                 src="/edit.png"
-                alt={`Edit result ${item.subject} `}
+                alt={`Edit event ${item.title} `}
                 width={16}
                 height={16}
               />
@@ -80,7 +72,7 @@ function ResultListPage() {
             <button className="w-7 h-7 rounded-full flex items-center justify-center bg-customPurple">
               <Image
                 src="/delete.png"
-                alt={`Delete exam ${item.subject}`}
+                alt={`Delete event ${item.title}`}
                 width={16}
                 height={16}
               />
@@ -94,7 +86,7 @@ function ResultListPage() {
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Results</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Events</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -112,10 +104,10 @@ function ResultListPage() {
           </div>
         </div>
       </div>
-      <Table columns={columns} renderRow={renderRow} data={resultsData} />
+      <Table columns={columns} renderRow={renderRow} data={eventsData} />
       <Pagination />
     </div>
   );
 }
 
-export default ResultListPage;
+export default EventListPage;
