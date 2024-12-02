48ad438223f40177853c369b63528ad7c43fbfa8
Marlena Broniewicz <bronka1.dev@gmail.com>
2024-12-02 16:31:38 +0100

    Added Prisma schema

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,13 +1,20 @@
 "use client";
 import Image from "next/image";
 import React, { useState } from "react";
-import TeacherForm from "./forms/TeacherForm";
+import dynamic from "next/dynamic";
+
+const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+  loading: () => <p>Loading...</p>,
+});
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <p>Loading...</p>,
+});
 
 const forms: {
   [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
 } = {
   teacher: (type, data) => <TeacherForm type={type} data={data} />,
-  student: (type, data) => <TeacherForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />,
 };
 
 type Form = {
