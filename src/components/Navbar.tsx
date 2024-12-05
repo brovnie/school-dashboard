adeec2b2b9d020a3f993742190fc28c209dbd490
Marlena Broniewicz <bronka1.dev@gmail.com>
2024-12-05 13:52:08 +0100

    Fixed styling height issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="flex-1 flex items-center justify-between p-4">
+    <div className=" flex items-center justify-between p-4">
       <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="search" width={14} height={14} />
         <input
