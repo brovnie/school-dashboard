cba908b56468bed8302e51b3b1f1d899c7560635
Marlena Broniewicz <bronka1.dev@gmail.com>
2024-12-06 16:36:43 +0100

    Lessons page - fetch data + search by title

--- src/app/list/teachers/[id]/page.tsx
+++ src/app/list/teachers/[id]/page.tsx
@@ -138,7 +138,10 @@ function SingleTeacherPage() {
             >
               Teacher&apos;s Students
             </Link>
-            <Link href="/" className="p-3 rounded-md bg-yellowLight">
+            <Link
+              href={`/list/lessons?teacherId=${"teacher2"}`}
+              className="p-3 rounded-md bg-yellowLight"
+            >
               Teacher&apos;s Lessons
             </Link>
             <Link href="/" className="p-3 rounded-md bg-pink-50">
