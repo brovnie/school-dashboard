b4cf444b57716db91eaae966124726fcdec12218
Marlena Broniewicz <bronka1.dev@gmail.com>
2024-12-06 13:12:27 +0100

    Filter teacher based on classId

--- src/app/list/teachers/page.tsx
+++ src/app/list/teachers/page.tsx
@@ -5,7 +5,7 @@ import TableSearch from "@/components/TableSearch";
 import { role, teachersData } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { Class, Subject, Teacher } from "@prisma/client";
+import { Class, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -102,8 +102,27 @@ async function TeacherListPage({
 }) {
   const { page, ...queryParams } = searchParams;
   const p = page ? parseInt(page) : 1;
+
+  const query: Prisma.TeacherWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "classId": {
+            query.lessons = {
+              some: {
+                classId: parseInt(value),
+              },
+            };
+          }
+        }
+      }
+    }
+  }
   const [data, count] = await prisma.$transaction([
     prisma.teacher.findMany({
+      where: query,
       include: {
         subjects: true,
         classes: true,
@@ -111,7 +130,9 @@ async function TeacherListPage({
       take: ITEM_PER_PAGE,
       skip: ITEM_PER_PAGE * (p - 1),
     }),
-    prisma.teacher.count(),
+    prisma.teacher.count({
+      where: query,
+    }),
   ]);
 
   return (
