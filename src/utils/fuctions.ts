3360a09d159df3ece00588ce22470d66704f8310
Marlena Broniewicz <bronka1.dev@gmail.com>
2025-01-13 15:18:30 +0100

    Removed unused import

--- src/utils/fuctions.ts
+++
@@ -1,7 +0,0 @@
-import { auth } from "@clerk/nextjs/server";
-
-export const getRole = async () => {
-  const { sessionClaims } = await auth();
-  const role = (sessionClaims?.metadata as { role?: string })?.role;
-  return role;
-};
