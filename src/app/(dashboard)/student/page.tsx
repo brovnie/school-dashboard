6b548a1e0fd38e27c25c9e89872a792730e50b2c
Marlena Broniewicz <bronka1.dev@gmail.com>
2025-01-16 19:44:49 +0100

    Show lesson from db in student page

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,23 +1,32 @@
-import Announcements from '@/components/Announcements'
-import BigCalendar from '@/components/BigCalendar'
-import EventCalendar from '@/components/EventCalendar'
-import React from 'react'
+import Announcements from "@/components/Announcements";
+import { BigCalendarContainer } from "@/components/BigCalendarContainer";
+import EventCalendar from "@/components/EventCalendar";
+import prisma from "@/lib/prisma";
+import { getUserId } from "@/lib/utils";
+import React from "react";
 
-const StudentPage = () => {
+const StudentPage = async () => {
+  const userId = await getUserId();
+  const classItem = await prisma.class.findMany({
+    where: {
+      students: { some: { id: userId! } },
+    },
+  });
+
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
-      <div className='w-full xl:w-2/3'>
-        <div className='h-full bg-white p-4 rounded-md'>
-          <h2 className='text-xl font-semibold'>Schedule (4A)</h2>
-          <BigCalendar/>
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h2 className="text-xl font-semibold">Schedule (4A)</h2>
+          <BigCalendarContainer type="classId" id={classItem[0].id} />
         </div>
       </div>
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
-      <EventCalendar/>
-      <Announcements/>
+        <EventCalendar />
+        <Announcements />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StudentPage
+export default StudentPage;
